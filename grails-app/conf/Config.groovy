e4a7cd6320e62a580ab859fe51b3e5e89ca0a2df
Cristyan Hernandez <deathkryz@gmail.com>
2019-06-22 10:36:19 -0700

    Initial commit

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -2,8 +2,8 @@ grails {
     mail {
         host = "smtp.gmail.com"
         port = 465
-        username = "mail@gmail.com"
-        password = "pass"
+        username = "sgaccesos@gmail.com"
+        password = "Xqd.2234"
         props = ["mail.smtp.auth":"true",
                  "mail.smtp.socketFactory.port":"465",
                  "mail.smtp.socketFactory.class":"javax.net.ssl.SSLSocketFactory",
